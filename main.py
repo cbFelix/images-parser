d04ab13398e6b4bdecb9f82c05d5dcbb3332a332
zxcat <97702830+cbFelix@users.noreply.github.com>
2023-10-22 19:08:56 +0500

    Add files via upload
    - Added the ability to use a proxy server (manually)
    - Added the ability to use classes that are used in HTML tags
    - The site parsing system has been changed
    - Changed information in logs
    - Added the ability to select the format in which the file will be saved

--- main.py
+++ main.py
@@ -1,38 +1,89 @@
 import tkinter as tk
 from tkinter import *
+from threading import Thread
+import os
 
 try:
     import parser
 
-except:
+except ValueError:
     print('Failed to import file "parser.py"!')
 
 row = 1.0
 
 
+def toggle_entry_state():
+    if var_tags.get() == 1:
+        entry_choice_class.configure(state="normal")
+    else:
+        entry_choice_class.configure(state="disabled")
+
+
+def proxy_state():
+    if var_proxy.get() == 1:
+        entry_choice_proxy_http.configure(state="normal")
+        entry_choice_proxy_https.configure(state="normal")
+    else:
+        entry_choice_proxy_http.configure(state="disabled")
+        entry_choice_proxy_https.configure(state="disabled")
+
+
 def download_images():
     global row
+
     row += 1
-    fr_logs_text.insert(str(row), f'{parser.check_response(entry_url.get())}\n')
-    print(parser.check_response(entry_url.get()))
+    fr_logs_text.insert(str(row), f'Parsing <{entry_url.get()}>...\n')
     url = entry_url.get()
     form = format_var.get()
+    path = None
+    headers = None
+
+    if var_tags.get() == 1:
+        tags = entry_choice_class.get()
+    else:
+        tags = None
     if entry_path_save.get() is None:
         path = "parsed_images"
     else:
         path = entry_path_save.get()
-    parser.download_images(url, path, format_save=form)
+
+    if entry_choice_proxy_https.get() is None or entry_choice_proxy_http.get() is None:
+        proxies = None
+
+    else:
+        proxies = {"http": entry_choice_proxy_http.get(), "https": entry_choice_proxy_https.get()}
+    if parser.check_response(url, proxies=proxies, headers=headers) is None:
+        fr_logs_text.insert(str(row), f'Error, code of response: {parser.check_response(url, proxies=proxies, headers=headers).status_code}\n')
+        print(parser.check_response(url, proxies=proxies, headers=headers))
+    else:
+        fr_logs_text.insert(str(row), f'Proxy: {proxies}\n')
+        fr_logs_text.insert(str(row), f'Headers: {headers}\n')
+        fr_logs_text.insert(str(row), f'Path: {path}\n')
+        fr_logs_text.insert(str(row), f'Code of response: {parser.check_response(url, proxies=proxies, headers=headers).status_code}\n')
+        fr_logs_text.insert(str(row), f'Downloading images from <{url}>...\n')
+        print(parser.check_response(url, proxies=proxies, headers=headers))
+        try:
+            parser.download_images(url=url, path=path, default_format=form, tags=tags, proxies=proxies, headers=headers)
+            fr_logs_text.insert(str(row), f'Parsing <{url}> completed!\n')
+        except Exception as e:
+            fr_logs_text.insert(str(row), f'Error: {e}\n')
+            print(e)
+
+
+def download_images_thread():
+    Thread(target=download_images).start()
 
 
 root = Tk()
 root.geometry('800x500')
 root.title('Images Parser by cbFelix')
 root.resizable(False, False)
+root.iconbitmap(None)
 
 fr_preview = Frame(root, width=796, height=2, borderwidth=1, relief='solid')
 fr_preview.pack(side=TOP)
 
-fr_main = Frame(root, width=783, height=130, borderwidth=1, relief='solid')
+fr_main = Frame(root, width=783, height=145, borderwidth=1, relief='solid')
 fr_main.pack(side=TOP, pady=1)
 fr_main.grid_propagate(False)
 
@@ -59,7 +110,7 @@ entry_path_save.grid(column=2, row=1, pady=10)
 lb_parse_path_info = Label(fr_main, name='lb_parse_path', text='*Default: this-directory/parsed_images/', width=35, font=('Arial', 10), fg='gray')
 lb_parse_path_info.grid(column=2, row=2, padx=3)
 
-parse_btn = Button(fr_main, name='parse button', text='Parse', command=download_images, width=5, font=('Arial', 10))
+parse_btn = Button(fr_main, name='parse button', text='Parse', command=download_images_thread, width=5, font=('Arial', 10))
 parse_btn.grid(column=3, row=0, padx=10, pady=10)
 
 format_var = StringVar(root)
@@ -68,7 +119,29 @@ formats = ["png", "jpg", "jpeg", "raw"]
 format_menu = OptionMenu(fr_main, format_var, *formats)
 format_menu.grid(column=4, row=0, padx=10)
 
-fr_console = Frame(root, width=783, height=200, borderwidth=1, relief='solid')
+entry_choice_class = Entry(fr_main, width=20, bg='white', state='disabled')
+entry_choice_class.grid(column=5, row=1)
+
+var_tags = IntVar()
+checkbox_class_allow = Checkbutton(fr_main, text="class", variable=var_tags, command=toggle_entry_state, onvalue=1, offvalue=0)
+checkbox_class_allow.grid(column=5, row=0, padx=10)
+
+lb_proxy_https = Label(fr_main, text='http: ', width=4, font=('Arial', 12))
+lb_proxy_https.grid(column=4, row=2)
+entry_choice_proxy_http = Entry(fr_main, width=20, bg='white', state='disabled')
+entry_choice_proxy_http.grid(column=5, row=2)
+
+lb_proxy_https = Label(fr_main, text='https: ', width=4, font=('Arial', 12))
+lb_proxy_https.grid(column=4, row=3)
+entry_choice_proxy_https = Entry(fr_main, width=20, bg='white', state='disabled')
+entry_choice_proxy_https.grid(column=5, row=3)
+
+var_proxy = IntVar()
+checkbox_allow_proxy = Checkbutton(fr_main, text="proxy", variable=var_proxy, command=proxy_state, onvalue=1, offvalue=0)
+checkbox_allow_proxy.grid(column=3, row=2, padx=10)
+
+
+fr_console = Frame(root, width=783, height=185, borderwidth=1, relief='solid')
 fr_console.pack(side=TOP, pady=1)
 fr_console.grid_propagate(False)
 fr_console.pack_propagate(False)
@@ -83,9 +156,9 @@ fr_footer = Frame(root, width=783, height=50, borderwidth=1, relief='solid')
 fr_footer.pack(side=TOP, pady=1)
 fr_footer.grid_propagate(False)
 
-lb_about = Label(fr_footer, text='Made by Victor "@cbFelix" Lebedev', font=('Arial', 14), width=34)
+lb_about = Label(fr_footer, text='Made by @cbFelix', font=('Arial', 14), width=34)
 lb_about.grid(column=0, row=0)
-lb_version = Label(fr_footer, text='Version: 1.0', font=('Arial', 14), width=15)
+lb_version = Label(fr_footer, text='Version: 1.1', font=('Arial', 14), width=15)
 lb_version.grid(column=1, row=0)
 
 root.mainloop()
--- parser.py
+++ parser.py
@@ -3,26 +3,30 @@ import random
 import string
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
-
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:109.0) Gecko/20100101 Firefox/118.0"
-}
-
-proxies = {
-    'http': '34.142.51.21P:443',
-    'https': '161.117.81.228:483'
-}
+from urllib.parse import urlparse, unquote
 
 
-def check_response(url):
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return response
+def check_response(url, proxies=None, headers=None):
+    if proxies is None:
+        proxies = None
+    else:
+        proxies = proxies
+    response = requests.get(url, proxies=proxies, headers=headers)
+    return response
 
 
-def download_images(url, path=None, format_save="png"):
-    response = requests.get(url, headers=headers)
+def download_images(url, path=None, default_format="png", tags=None, proxies=None, headers=None):
+
+    if proxies is None:
+        proxies = None
+    else:
+        proxies = proxies
+
+    if proxies is None:
+        response = requests.get(url, headers=headers)
+    else:
+        response = requests.get(url, proxies=proxies, headers=headers)
+
     soup = BeautifulSoup(response.content, "html.parser")
 
     if path is None:
@@ -31,26 +35,45 @@ def download_images(url, path=None, format_save="png"):
     if not os.path.exists(path):
         os.makedirs(path)
 
-    images = soup.find_all("img")
+    if tags is None:
+        images = soup.find_all("img")
+    else:
+        images = soup.find_all("img", f'{tags}')
 
     for img in images:
-        img_url = img["src"]
+        if "data-original" in img.attrs:
+            img_url = img["data-original"]
+        elif "data-src" in img.attrs:
+            img_url = img["data-src"]
+        else:
+            img_url = img["src"]
 
         if not urlparse(img_url).netloc:
             img_url = url + img_url
 
         random_name = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
+
+        parsed_url = urlparse(img_url)
+        image_path = unquote(parsed_url.path)
+        _, image_ext = os.path.splitext(image_path)
+        if image_ext:
+            format_save = image_ext[1:]
+        else:
+            format_save = default_format
+
         file_name = f"{path}/{random_name}.{format_save}"
+
         try:
-            image_data = requests.get(img_url, headers=headers).content
+            if proxies is None:
+                image_data = requests.get(img_url, headers=headers).content
+            else:
+                image_data = requests.get(img_url, headers=headers, proxies=proxies, timeout=10).content
 
         except:
-            try:
-                image_data = requests.get(img_url, headers=headers).content
+            image_data = None
 
-            except:
-                image_data = requests.get(f"https:{img_url}", headers=headers, timeout=10).content
+        if image_data:
+            with open(file_name, "wb") as f:
+                f.write(image_data)
 
-
-        with open(file_name, "wb") as f:
-            f.write(image_data)
+    return 'Done'
